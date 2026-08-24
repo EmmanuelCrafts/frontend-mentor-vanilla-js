df91ea07ffaf4e780afd62a1721e4c8a37eca949
Emmanuel Oginga <ogolemmanuel3@gmail.com>
2026-08-24 22:52:46 +0300

    feat: add win message display and update score tracking for Tic-Tac-Toe

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -44,8 +44,14 @@ function changePlayerTitles() {
 // game logic
 let currentPlayer = 'X';
 let board = ['','','','','','','','',''];
-
 const cells = document.querySelectorAll('.cell');
+    let winCard = document.querySelector('.win-container');
+    let xWins = document.querySelector('.X-wins');
+    let oWins = document.querySelector('.O-wins');
+    let draws = document.querySelector('.draws');
+    let title = document.querySelector('.win-title');
+    let roundText = document.querySelector('.round-text');
+    let winIcon = document.querySelector('.win-icon');
 
 cells.forEach(cell => {
     cell.addEventListener('click', () => {
@@ -63,8 +69,51 @@ cells.forEach(cell => {
              img.alt = "O";
          }
            img.classList.add('show');
+
+      checkWinner();
+      if(checkDraw()) {
+        winCard.classList.remove('hidden');
+        draws.textContent = Number(draws.textContent) + 1;
+        roundText.textContent = `ROUND TIED`;
+        roundText.classList.add('draws');
+        title.classList.add('hidden');
+        winIcon.classList.add('hidden');
+      }
       // switch player
        currentPlayer = currentPlayer === "X" ? "O" : "X";
     });
 });
+function checkWinner () {
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
 
+   winConditions.forEach(win => {
+     const [a, b ,c ] = win;
+
+     if(board[a] && board[a] === board[b] && board[b] === board[c]) {
+        winCard.classList.remove('hidden');
+        
+        if (board[a] === "X") {
+            winIcon.src = "assets/icon-x.svg"
+            xWins.textContent = Number(xWins.textContent) + 1;
+        } else {
+            winIcon.src =  "assets/icon-o.svg"
+            title.textContent = "OH NO, YOU LOST...";
+            roundText.classList.add('o-wins');
+            oWins.textContent = Number(oWins.textContent) + 1;
+        }
+     }
+   })
+}
+
+function checkDraw() {
+  return board.every(cell => cell !== '');
+}
