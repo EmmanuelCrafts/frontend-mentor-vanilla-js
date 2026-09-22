d798a682f6391d3a490b6200ee1273b8a97d6630
Emmanuel Oginga <ogolemmanuel3@gmail.com>
2026-09-22 21:49:47 +0300

    feat: update game screen layout and add category selection functionality

--- hangman-game/script.js
+++ hangman-game/script.js
@@ -5,10 +5,21 @@ const categoryScreen = document.querySelector('.category-screen');
 const startGameBtn = document.querySelector('.play');
 const playInfoBtn = document.querySelector('.play-info');
 const backBtn = document.querySelectorAll('.back-container');
+const category = document.querySelectorAll('.category-btn');
+
+// game variables
+let allCategories = null;
+
 // Event Listeners
 playInfoBtn.addEventListener("click", showStepsScreen);
 startGameBtn.addEventListener("click", showCategoryScreen);
 backBtn.forEach(button => button.addEventListener("click", showStartGameScreen));
+category.forEach(category => category.addEventListener("click", function(){
+    let name = category.textContent.toLowerCase();
+  
+}));
+
+
 function showStepsScreen () {
    startGameScreen.classList.add('hidden');
    stepsScreen.classList.remove('hidden')
@@ -23,3 +34,26 @@ function showCategoryScreen () {
    startGameScreen.classList.add('hidden');
    categoryScreen.classList.remove('hidden')
 }
+
+
+
+
+async function getCategories() {
+    try {
+        const response = await fetch('/data.json')
+        const data = await response.json();
+        return data.categories;
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function loadCategories() {
+    allCategories = await getCategories();
+    console.log(allCategories);
+}
+
+loadCategories();
+
+
