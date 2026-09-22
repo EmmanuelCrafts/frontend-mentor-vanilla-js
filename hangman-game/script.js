6150414d4d4e5c7c6132a39ba04b88c9687d41a8
Emmanuel Oginga <ogolemmanuel3@gmail.com>
2026-09-22 23:34:49 +0300

    feat: add game screen functionality and enhance word display logic

--- hangman-game/script.js
+++ hangman-game/script.js
@@ -2,10 +2,12 @@
 const startGameScreen = document.querySelector('.start-game-screen');
 const stepsScreen = document.querySelector('.steps-screen');
 const categoryScreen = document.querySelector('.category-screen');
+const gameScreen = document.querySelector('.game-screen');
 const startGameBtn = document.querySelector('.play');
 const playInfoBtn = document.querySelector('.play-info');
 const backBtn = document.querySelectorAll('.back-container');
 const category = document.querySelectorAll('.category-btn');
+const wordDisplay = document.querySelector('.word-display');
 
 // game variables
 let allCategories = null;
@@ -15,6 +17,7 @@ playInfoBtn.addEventListener("click", showStepsScreen);
 startGameBtn.addEventListener("click", showCategoryScreen);
 backBtn.forEach(button => button.addEventListener("click", showStartGameScreen));
 category.forEach(category => category.addEventListener("click", function(){
+    showGameScreen();
     let name = category.textContent.toLowerCase();
     displayCategoryItems(name);
 }));
@@ -35,8 +38,10 @@ function showCategoryScreen () {
    categoryScreen.classList.remove('hidden')
 }
 
-
-
+function showGameScreen () {
+   categoryScreen.classList.add('hidden');
+   gameScreen.classList.remove('hidden')
+}
 
 async function getCategories() {
     try {
@@ -78,6 +83,18 @@ function randomCategoryItem(arr) {
 }
 
 function displayItem(item) {
-  const wordArr = item.toUpperCase().replaceAll(" ", "").split('');
+  const wordArr = item.toUpperCase().split('');
   console.log(wordArr);
+  wordArr.forEach(letter => {
+      const div = document.createElement('div');
+      div.textContent = letter;
+
+      if (letter === " ") {
+        div.classList.add('space');
+      }
+
+      div.classList.add('letter')
+      wordDisplay.appendChild(div);
+  })
+
 }
